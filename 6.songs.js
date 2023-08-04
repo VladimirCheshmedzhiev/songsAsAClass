33bb0699303fae7a6a54d4e4a869a3f3a1530268
Vlad <131590167+VladimirCheshmedzhiev@users.noreply.github.com>
2023-08-04 23:42:54 +0300

    Add files via upload

---
+++ 6.songs.js
@@ -0,0 +1,34 @@
+function songs(input) {
+
+    class Song {
+        constructor(typeList, name, time) {
+            this.typeList = typeList;
+            this.name = name;
+            this.time = time;
+        }
+    }
+
+    let numberOfSongs = input.shift();
+    let typeSong = input.pop();
+    let songs = [];
+
+    for (let i = 0; i < numberOfSongs; i++) {
+        let [type, name, time] = input[i].split('_');
+        let song = new Song(type, name, time);
+        songs.push(song);
+    }
+
+    if (typeSong === "all") {
+        songs.forEach((i) => console.log(i.name));
+    } else {
+        let filtered = songs.filter((i) => i.typeList === typeSong);
+        filtered.forEach((i) => console.log(i.name));
+    }
+}
+songs([
+    3,
+    'favourite_DownTown_3:14',
+    'favourite_Kiss_4:16',
+    'favourite_Smooth Criminal_4:01',
+    'favourite'
+]);
